d450492c44fc89a848646dd1d22418f2a03687ef
Ulyana Spiridonova <ulyana@Ulyanas-MacBook-Pro.local>
2018-10-27 17:46:33 +0300

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const path = require("path"),
   express = require("express"),
   http = require("http"),
-  socketIO = require("socket.io");
+  socketIO = require("socket.io"),
+  { generateMessage } = require("./utils/message");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
@@ -11,30 +12,19 @@ const io = socketIO(server);
 
 io.on("connection", socket => {
   console.log("new user connected");
-  socket.emit("newMessage", {
-    form: "Admin",
-    text: "Welcome to the chat app",
-    createdAt: new Date().getTime().toString()
-  });
+  socket.emit(
+    "newMessage",
+    generateMessage("Admin", "Welcome to the chat app")
+  );
 
-  socket.broadcast.emit("newMessage", {
-    from: "Admin",
-    text: "New user joined",
-    createdAt: new Date().getTime().toString()
-  });
+  socket.broadcast.emit(
+    "newMessage",
+    generateMessage("Admin", "New user joined")
+  );
 
   socket.on("createdMessage", message => {
     console.log(message);
-    io.emit("newMessage", {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime().toString()
-    });
-    // socket.broadcast.emit("newMessage", {
-    //   from: "server",
-    //   text: "broadcasting",
-    //   createdAt: new Date().toLocaleDateString()
-    // });
+    io.emit("newMessage", generateMessage(message.from, message.text));
   });
 });
 
