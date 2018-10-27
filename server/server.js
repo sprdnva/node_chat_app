f833fe4165575c23a0ae5855ebf079c24eaf60b3
Ulyana Spiridonova <ulyana@Ulyanas-MacBook-Pro.local>
2018-10-27 16:30:13 +0300

    Add socketIO connect event handler

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const path = require("path"),
   express = require("express"),
-  app = express();
+  http = require("http"),
+  socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", socket => {
+  console.log("new user connected");
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port);
+server.listen(port);
